57c70ad1ba8a2368c40fa39a4de9baef76a253a1
Abdurrahman075 <fayyadh.abdurrahman007@gmail.com>
2020-09-10 21:45:23 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config(); // Memuat file .env
+const { Client } = require('@androz2091/insta.js');
+const { readdirSync } = require('fs');
+
+const client = new Client({ disableReplyPrefix: true });
+
+readdirSync('./src/handler/').forEach(file => {
+    client.on(file.split('.')[0], (...args) => require(`./handler/${file}`)(client, ...args));
+});
+
+client.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
